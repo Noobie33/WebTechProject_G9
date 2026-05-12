002f0b15d620d68dbf5969afd760f378ad1473a7
Noobie33 <setu7090@gmail.com>
2026-05-12 23:01:57 +0600

    add placeholder pages for future modules

---
+++ View/SellerDashboard.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+$isloggedIn=$_SESSION["loggedIn"] ?? false;
+if(!$isloggedIn)
+    {
+        Header("Location:Login.php"); 
+    }
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="Design/Style.css">
+    </head>
+    <body>
+        <?php
+            echo "<h1>Seller Dashboard Page</h1>";
+            echo "This page will be completed in another task.";
+        ?>
+        <br><br>
+        <a href="Dashboard.php">Back To Dashboard</a>
+    </body>
+</html>
