1a72e1cdf70d8b8a6aa425c87874ebe07fe7e936
Noobie33 <setu7090@gmail.com>
2026-05-16 02:08:54 +0600

    Add "My Bids" link to navigation in BecomeSeller, BrowseAuctions, and Profile pages

--- View/BrowseAuctions.php
+++ View/BrowseAuctions.php
@@ -32,6 +32,7 @@ if(!$isloggedIn)
             <a href="Dashboard.php">Dashboard</a>
             <a href="Profile.php">Profile</a>
             <a href="BrowseAuctions.php">Browse Auctions</a>
+            <a href="MyBids.php">My Bids</a>
             <a href="CreateListing.php">Create Listing</a>
             <a href="SellerDashboard.php">Seller Dashboard</a>
             <a href="../Controller/Logout.php">Logout</a>
@@ -45,6 +46,7 @@ if(!$isloggedIn)
             <a href="Profile.php">Profile</a>
             <a href="BecomeSeller.php">Become Seller</a>
             <a href="BrowseAuctions.php">Browse Auctions</a>
+            <a href="MyBids.php">My Bids</a>
             <a href="../Controller/Logout.php">Logout</a>
         </div>
         <?php
